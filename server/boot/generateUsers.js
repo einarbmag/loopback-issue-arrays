2b899d0deb5272b9004b9bca7dae4b718ba49558
Einar Bui Magnusson <einarbmag@gmail.com>
2015-08-20 18:07:57 +0100

    Improved example to show that it's only the promisified way that fails, not the callback way

--- server/boot/generateUsers.js
+++ server/boot/generateUsers.js
@@ -8,7 +8,8 @@ module.exports = function generateUsers(app){
     .then(function(response){
         console.log(response);
 
-        return User.create([{'username':'tester2','email':'test2@test.com', 'password':'testing'}, {'username':'tester3','email':'test3@test.com', 'password':'testing'}]);
+        return User.create([{'username':'tester2','email':'test2@test.com', 'password':'testing'}, 
+                            {'username':'tester3','email':'test3@test.com', 'password':'testing'}]);
     }) 
     .then(function(response){
         console.log(response)
@@ -17,5 +18,18 @@ module.exports = function generateUsers(app){
         console.log('error:')
         console.log(error)
     })
+
+    setTimeout(function(){
+
+        User.create({'username':'tester4','email':'test4@test.com','password':'testing'}, function(err, response){
+            console.log(response);
+            User.create([{'username':'tester5','email':'test5@test.com', 'password':'testing'}, 
+                            {'username':'tester6','email':'test6@test.com', 'password':'testing'}], function(err, response){
+                                console.log(response)
+                            });
+        })
+
+    }, 2000);
+    
 
 }
